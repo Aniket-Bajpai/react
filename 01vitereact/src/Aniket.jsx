9c4af983cefdcd8bd461d770c4948fd151729f44
Aniket Kumar Bajpai <aniketbajpai@Anikets-MacBook-Air.local>
2025-08-27 18:42:18 +0530

    Added first Project

---
+++ 01vitereact/src/Aniket.jsx
@@ -0,0 +1,9 @@
+function Aniket(){
+    return(
+        <div>
+            <h3>Aniket React Learning</h3>
+        </div>
+    )
+}
+
+export default Aniket
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Aniket from "./Aniket.jsx"
+
+function App() {
+  return (
+    <>
+        <Aniket />
+        <h1>This is react | Aniket</h1>
+    </>
+  )
+}
+
+export default App
